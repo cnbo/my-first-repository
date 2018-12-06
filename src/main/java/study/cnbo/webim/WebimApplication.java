8696a16e62f985b227efcdce884aba2bc514eea7
cnbo <18163912002@163.com>
2018-12-06 23:27:45 +0800

    init

---
+++ src/main/java/study/cnbo/webim/WebimApplication.java
@@ -0,0 +1,12 @@
+package study.cnbo.webim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebimApplication.class, args);
+	}
+}
---
+++ src/test/java/study/cnbo/webim/WebimApplicationTests.java
@@ -0,0 +1,16 @@
+package study.cnbo.webim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebimApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
